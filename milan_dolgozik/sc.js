df347d5694dbef06731a338cb00292cf288a1b16
Miloka255 <153728902+Miloka255@users.noreply.github.com>
2024-10-20 16:00:56 +0200

    dffde
    fdfd

--- milan_dolgozik/sc.js
+++ milan_dolgozik/sc.js
@@ -4,3 +4,14 @@ function toggleDropdown(event) {
     dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
 }
 
+
+document.addEventListener('click', function(event) {
+    const isDropdownToggle = event.target.matches('.dropdown-toggle');
+    const dropdownMenus = document.querySelectorAll('.dropdown-menu');
+
+    if (!isDropdownToggle) {
+        dropdownMenus.forEach(menu => {
+            menu.style.display = 'none'; 
+        });
+    }
+});
