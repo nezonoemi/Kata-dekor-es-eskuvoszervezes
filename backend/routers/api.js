e65a53c7ef3fb427ef29bdea1f53dfdd4d60056c
Koliver05 <kozmao2005@gmail.com>
2024-12-11 21:45:05 +0100

    api.js keresztül lehet majd kérdezéseket végrehajtani és stb

--- backend/routers/api.js
+++ backend/routers/api.js
@@ -2,17 +2,40 @@ import express from 'express';
 import pool from '../config/db.js';
 
 const router = express.Router();
+router.get("/teachers/:id", async (req, res) =>{
+    try{
+         //kiszedtük az id-t params taralmazza az utvonal paramétereket lehet több is akár
+         //a szöveget át konvertálja hogy 1 number legyen azért 
+         //kell mert az adatbázisnak szám kell minde féle képpen
+        let id = parseInt(req.params.id); 
+        // ha a parseInt-et nem sikerült át konvertálni és ha isNan(id) akkor nem szám
+        if(isNaN(id)){
+            throw new Error("Parameter 'id' must be a valid integer");
+        }
+        // nem lehet nulla az idnél
+        if(id < 0){
+            throw new Error("Parameter 'id' must be greater than 0");
+        }
+        // kérdőjel helyére mit illesztjen be --> [id]
+        let [result, ] = await pool.query("SELECT * FROM teachers WHERE id = ?;", [id]);
 
-// Példa: teachers lekérése az adatbázisból
-router.get('/teahers', async (req, res) => {
-    try {
-        const [rows] = await pool.query('SELECT * FROM teachers');
-        res.json(rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Database query failed' });
+        res.json(result);
+        
+    }catch(err){
+        //hibákat ellenörzünk ugyan ez mint ez ellőti
+        if(err.message.includes("Parameter 'id'")){
+            res.json({
+                "error" : err.message
+            });
+            return;
+        }
+        //fallback ugyan az mint ez ellőti
+        res.status(500).json({
+            "error" : "Couldn't query teachers"
+        });
     }
 });
+
 
 // Például egy API az oldalak adataival
 router.get('/pages', (req, res) => {
