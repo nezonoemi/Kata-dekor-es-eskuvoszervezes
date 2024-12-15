bb58c33f58d07cf7322ff229ae4c38efc4287640
Koliver05 <kozmao2005@gmail.com>
2024-12-15 21:43:03 +0100

    localhoston fut a szerver

--- backend/routers/pages.js
+++ backend/routers/pages.js
@@ -4,46 +4,41 @@ import path from 'path';
 const router = express.Router();
 const __dirname = path.resolve();
 
+// Statikus fájlok szolgáltatása (HTML fájlok)
+router.use(express.static(path.join(__dirname, '..', 'frontend', 'src', 'html')));
+
 // Dinamikus oldalak
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..','frontend','src','html','index.html'));
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'src', 'html', 'index.html'));
 });
 
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..','frontend','src','html', 'autodekoracio.html'));
+// Oldalak dinamikusan
+const pages = [
+    'autodekoracio',
+    'bemutatotermunk',
+    'egyeb',
+    'eskuvoszervezes',
+    'kapcsolat',
+    'order',
+    'rendezvenyszervezes',
+    'szezonalistermekek',
+    'teremdiszites',
+    'viragkoteszet',
+    'berelhetotermekek',
+    'arajanlatkeres'
+];
+
+// Dinamikus útvonalak létrehozása
+pages.forEach(page => {
+    router.get(`/${page}`, (req, res) => {
+        // A page változó értéke alapján dinamikusan adunk vissza HTML fájlt
+        res.sendFile(path.join(__dirname, '..', 'frontend', 'src', 'html', `${page}.html`));
+    });
 });
 
+// Minden egyéb kérésre alapértelmezett oldal visszaadása
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..','frontend','src','html', 'bemutatotermunk.html'));
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'src', 'html', 'index.html'));
 });
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'..', 'frontend','src','html', 'egyeb.html'));
-});
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend','src','html', 'eskuvoszervezes.html'));
-});
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'..', 'frontend', 'src','html','kapcsolat.html'));
-});
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'..', 'frontend','src','html', 'order.html'));
-});
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..','frontend','src','html', 'rendezvenyszervezes.html'));
-});
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'..', 'frontend', 'src','html','szezonalistermekek.html'));
-});
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..','frontend','src','html', 'teremdiszites.html'));
-});
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend','src','html', 'viragkoteszet.html'));
-});
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend','src','html', 'berelhetotermekek.html'));
-});
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend','src','html', 'arajanlatkeres.html'));
-});
+
 export default router;
