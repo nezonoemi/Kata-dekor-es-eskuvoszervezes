488ac15ee85d579064af828599c3c061b15449a4
Koliver05 <kozmao2005@gmail.com>
2024-12-15 21:33:10 +0100

    router elérések local host valtozik a cim

--- backend/routers/pages.js
+++ backend/routers/pages.js
@@ -9,41 +9,41 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..','frontend','src','html','index.html'));
 });
 
-router.get('/autodekoracio', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..','frontend','src','html', 'autodekoracio.html'));
 });
 
-router.get('/bemutatotermunk', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..','frontend','src','html', 'bemutatotermunk.html'));
 });
-router.get('/egyeb', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,'..', 'frontend','src','html', 'egyeb.html'));
 });
-router.get('/eskuvoszervezes', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'frontend','src','html', 'eskuvoszervezes.html'));
 });
-router.get('/kapcsolat', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,'..', 'frontend', 'src','html','kapcsolat.html'));
 });
-router.get('/order', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,'..', 'frontend','src','html', 'order.html'));
 });
-router.get('/rendezvenyszervezes', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..','frontend','src','html', 'rendezvenyszervezes.html'));
 });
-router.get('/szezonalistermekek', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,'..', 'frontend', 'src','html','szezonalistermekek.html'));
 });
-router.get('/teremdiszites', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..','frontend','src','html', 'teremdiszites.html'));
 });
-router.get('/viragkoteszet', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'frontend','src','html', 'viragkoteszet.html'));
 });
-router.get('/berelhetotermekek', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'frontend','src','html', 'berelhetotermekek.html'));
 });
-router.get('/arajanlatkeres', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'frontend','src','html', 'arajanlatkeres.html'));
 });
 export default router;
