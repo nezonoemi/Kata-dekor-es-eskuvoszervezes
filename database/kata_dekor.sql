694b364a9650060b78da78428251ecc34014567f
Koliver05 <kozmao2005@gmail.com>
2025-02-25 16:46:25 +0100

    adatbázis sablon

---
+++ database/kata_dekor.sql
@@ -0,0 +1,124 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2025. Feb 25. 16:44
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `kata_dekor`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `ajanlatkeres`
+--
+
+CREATE TABLE `ajanlatkeres` (
+  `ajanlatkeres_id` int(11) NOT NULL,
+  `vezeteknev` varchar(255) DEFAULT NULL,
+  `keresztnev` varchar(255) DEFAULT NULL,
+  `ajanlatkeres_email` varchar(255) DEFAULT NULL,
+  `ajanlatkeres_megjegyzes` varchar(255) DEFAULT NULL,
+  `termek_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `berelhetotermekek`
+--
+
+CREATE TABLE `berelhetotermekek` (
+  `berelheto_id` int(11) NOT NULL,
+  `termeknev` varchar(255) DEFAULT NULL,
+  `termekar` int(11) NOT NULL,
+  `termekleiras` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `felhasznalo`
+--
+
+CREATE TABLE `felhasznalo` (
+  `id` int(11) NOT NULL,
+  `vezeteknev` varchar(255) DEFAULT NULL,
+  `keresztnev` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `megjegyzes` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `ajanlatkeres`
+--
+ALTER TABLE `ajanlatkeres`
+  ADD PRIMARY KEY (`ajanlatkeres_id`),
+  ADD KEY `termek_id` (`termek_id`);
+
+--
+-- A tábla indexei `berelhetotermekek`
+--
+ALTER TABLE `berelhetotermekek`
+  ADD PRIMARY KEY (`berelheto_id`);
+
+--
+-- A tábla indexei `felhasznalo`
+--
+ALTER TABLE `felhasznalo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `ajanlatkeres`
+--
+ALTER TABLE `ajanlatkeres`
+  MODIFY `ajanlatkeres_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `berelhetotermekek`
+--
+ALTER TABLE `berelhetotermekek`
+  MODIFY `berelheto_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `felhasznalo`
+--
+ALTER TABLE `felhasznalo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `berelhetotermekek`
+--
+ALTER TABLE `berelhetotermekek`
+  ADD CONSTRAINT `berelhetotermekek_ibfk_1` FOREIGN KEY (`berelheto_id`) REFERENCES `ajanlatkeres` (`termek_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
