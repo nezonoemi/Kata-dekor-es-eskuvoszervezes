7fe720e912053c04a123ee740b83022a6d906991
Koliver05 <kozmao2005@gmail.com>
2025-03-02 10:35:19 +0100

    adatbázis  dauttomatikus idők ejelenek miden beszúrás vagyódosítás esetén

--- database/kata_dekor.sql
+++ database/kata_dekor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Feb 28. 10:25
+-- Létrehozás ideje: 2025. Már 02. 10:34
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -24,54 +24,59 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `ajanlatkeres`
+-- Tábla szerkezet ehhez a táblához `order`
 --
 
-CREATE TABLE `ajanlatkeres` (
-  `ajanlatkeres_id` int(11) NOT NULL,
-  `vezeteknev` varchar(255) DEFAULT NULL,
-  `keresztnev` varchar(255) DEFAULT NULL,
-  `ajanlatkeres_email` varchar(255) DEFAULT NULL,
-  `ajanlatkeres_megjegyzes` varchar(255) DEFAULT NULL,
-  `termek_id` int(11) NOT NULL
+CREATE TABLE `order` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `rentable_id` int(11) NOT NULL,
+  `create_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `update_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(50) DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `berelhetotermekek`
+-- Tábla szerkezet ehhez a táblához `quote_request`
 --
 
-CREATE TABLE `berelhetotermekek` (
-  `berelheto_id` int(11) NOT NULL,
-  `termeknev` varchar(255) DEFAULT NULL,
-  `termekar` int(11) NOT NULL,
-  `termekleiras` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `felhasznalo`
---
-
-CREATE TABLE `felhasznalo` (
-  `id` int(11) NOT NULL,
-  `vezeteknev` varchar(255) DEFAULT NULL,
-  `keresztnev` varchar(255) DEFAULT NULL,
+CREATE TABLE `quote_request` (
+  `quote_request_id` int(11) NOT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `telefonszam` int(11) NOT NULL,
-  `jelszo` varchar(255) DEFAULT NULL
+  `note` varchar(255) DEFAULT NULL,
+  `product_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `rendeles`
+-- Tábla szerkezet ehhez a táblához `rentable_products`
 --
 
-CREATE TABLE `rendeles` (
-  `rendeles_id` int(11) NOT NULL
+CREATE TABLE `rentable_products` (
+  `rentable_id` int(11) NOT NULL,
+  `product_name` varchar(255) DEFAULT NULL,
+  `product_price` int(11) NOT NULL,
+  `product_description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone_number` int(11) NOT NULL,
+  `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
@@ -79,67 +84,70 @@ CREATE TABLE `rendeles` (
 --
 
 --
--- A tábla indexei `ajanlatkeres`
+-- A tábla indexei `order`
 --
-ALTER TABLE `ajanlatkeres`
-  ADD PRIMARY KEY (`ajanlatkeres_id`),
-  ADD KEY `termek_id` (`termek_id`);
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `rentable_id` (`rentable_id`);
 
 --
--- A tábla indexei `berelhetotermekek`
+-- A tábla indexei `quote_request`
 --
-ALTER TABLE `berelhetotermekek`
-  ADD PRIMARY KEY (`berelheto_id`);
+ALTER TABLE `quote_request`
+  ADD PRIMARY KEY (`quote_request_id`),
+  ADD KEY `product_id` (`product_id`);
 
 --
--- A tábla indexei `felhasznalo`
+-- A tábla indexei `rentable_products`
 --
-ALTER TABLE `felhasznalo`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `rentable_products`
+  ADD PRIMARY KEY (`rentable_id`);
 
 --
--- A tábla indexei `rendeles`
+-- A tábla indexei `user`
 --
-ALTER TABLE `rendeles`
-  ADD PRIMARY KEY (`rendeles_id`);
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- A kiírt táblák AUTO_INCREMENT értéke
 --
 
 --
--- AUTO_INCREMENT a táblához `ajanlatkeres`
+-- AUTO_INCREMENT a táblához `order`
 --
-ALTER TABLE `ajanlatkeres`
-  MODIFY `ajanlatkeres_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT a táblához `berelhetotermekek`
+-- AUTO_INCREMENT a táblához `quote_request`
 --
-ALTER TABLE `berelhetotermekek`
-  MODIFY `berelheto_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `quote_request`
+  MODIFY `quote_request_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT a táblához `felhasznalo`
+-- AUTO_INCREMENT a táblához `rentable_products`
 --
-ALTER TABLE `felhasznalo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `rentable_products`
+  MODIFY `rentable_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT a táblához `rendeles`
+-- AUTO_INCREMENT a táblához `user`
 --
-ALTER TABLE `rendeles`
-  MODIFY `rendeles_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Megkötések a kiírt táblákhoz
 --
 
 --
--- Megkötések a táblához `berelhetotermekek`
+-- Megkötések a táblához `order`
 --
-ALTER TABLE `berelhetotermekek`
-  ADD CONSTRAINT `berelhetotermekek_ibfk_1` FOREIGN KEY (`berelheto_id`) REFERENCES `ajanlatkeres` (`termek_id`);
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `order_ibfk_2` FOREIGN KEY (`rentable_id`) REFERENCES `rentable_products` (`rentable_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
