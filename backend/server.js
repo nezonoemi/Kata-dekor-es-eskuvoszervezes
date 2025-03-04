b02cf14961f749a4c300d7915db8cd35cfc9b8a3
Koliver05 <kozmao2005@gmail.com>
2025-03-04 13:09:26 +0100

    mod

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,18 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser"; 
 import apiRoutes from "./routers/api.js";
 
 const app = express();
 const PORT = process.env.PORT || 3443;
 
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
 app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
-    console.log(`HTTP szerver fut a ${PORT} porton`);
+    console.log(` HTTP szerver fut a ${PORT} porton`);
 });
